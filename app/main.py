a14adfbf9801ea82983a0507ccc39bb63c5e69be
John Fizer <fizerco@gmail.com>
2026-01-07 03:00:36 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -22,8 +22,11 @@ def read_root():
 def health_check():
     return {"status": "ok", "db": "connected"}
 
+import uvicorn
+import os 
+
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("app.main:app",
-                host="0.0.0.0", port = int(os.getenv("PORT",8000)), reload = True)
+    port = int(os.getenv("PORT",8000))
+    uvicorn.run("app.main:app", host="0.0.0.0", , reload = True)
+
 
